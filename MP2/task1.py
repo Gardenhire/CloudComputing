e2ce913629c177c8ff4e7cbe0fe6ad75960452b2
Kevin <kgardenhire16@gmail.com>
2024-01-24 22:36:26 -0600

    added task1

---
+++ MP2/task1.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify, request
+
+
+
+app = Flask(__name__)
+
+global seedNumber
+seedNumber = '0'
+
+@app.route('/', methods = ['GET']) 
+def home(): 
+    global seedNumber
+    return str(seedNumber)
+    
+@app.route('/', methods = ['POST']) 
+def post():
+    global seedNumber
+    content = request.get_json(silent=True)
+    seedNumber = content['num']
+    return str(seedNumber)
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
