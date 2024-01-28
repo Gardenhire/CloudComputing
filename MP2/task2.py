1365918ab8254975752123ddc4e6495f2bfa4b52
Kevin <kgardenhire16@gmail.com>
2024-01-27 21:11:00 -0600

    added task2

---
+++ MP2/task2.py
@@ -0,0 +1,24 @@
+from flask import Flask,jsonify, request
+import subprocess
+import socket
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET']) 
+def home(): 
+    return str(socket.gethostname() )
+    
+@app.route('/', methods = ['POST']) 
+def post():
+    subprocess.Popen('python3 stress.py')
+    return str("0")
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(host='0.0.0.0',port=5000)
